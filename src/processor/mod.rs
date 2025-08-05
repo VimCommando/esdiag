40ee2a99291aeecaefaf817a43f809dd0ca3cadd
Ryan Eno <ryan.eno@elastic.co>
2025-08-05 12:50:52 -0700

    Update default kibana filter to 90d

--- src/processor/mod.rs
+++ src/processor/mod.rs
@@ -89,7 +89,7 @@ impl Diagnostic {
         );
         if let Ok(kibana_url) = std::env::var("ESDIAG_KIBANA_URL") {
             let kibana_link = format!(
-                "{}/app/dashboards#/view/4e0a26b2-e5f8-4b58-b617-86f5cdd0edad?_g=(filters:!(('$state':(store:globalState),meta:(disabled:!f,index:'4319ebc4-df81-4b18-b8bd-6aaa55a1fd13',key:diagnostic.id,negate:!f,params:(query:'{}'),type:phrase),query:(match_phrase:(diagnostic.id:'{}')))),refreshInterval:(pause:!t,value:60000),time:(from:now-7d,to:now))",
+                "{}/app/dashboards#/view/4e0a26b2-e5f8-4b58-b617-86f5cdd0edad?_g=(filters:!(('$state':(store:globalState),meta:(disabled:!f,index:'4319ebc4-df81-4b18-b8bd-6aaa55a1fd13',key:diagnostic.id,negate:!f,params:(query:'{}'),type:phrase),query:(match_phrase:(diagnostic.id:'{}')))),refreshInterval:(pause:!t,value:60000),time:(from:now-90d,to:now))",
                 kibana_url, report.metadata.id, report.metadata.id
             );
             log::info!("{}", kibana_link);
